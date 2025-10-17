27f0bfd5d7af24e638e00935f0d851cbd4d616cc
hafsah-tabassum971 <hafsahanif567@gmail.com>
2025-10-17 17:21:31 +0500

    Updated

--- yoga-landing-page/src/components/Hero.jsx
+++ yoga-landing-page/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "./Nav";
-import heroImage from "../assets/hero.webp";
+import heroImage from "/hero.webp";
 
 const Hero = () => {
   const handleScroll = (e, href) => {
